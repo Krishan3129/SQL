35504beca6330a4bec7bfa9044bbecd8fc97cfbe
Krishan Kumar Choudhary <140525583+Krishan3129@users.noreply.github.com>
2024-11-17 17:50:01 +0530

    Update mysolution_06.sql

--- SQL_exercise_06/mysolution_06.sql
+++ SQL_exercise_06/mysolution_06.sql
@@ -3,16 +3,25 @@
     -- and the hours worked by that scientist on each project, 
     -- in alphabetical order of project name, then scientist name.
     
-select s.Name, p.Name, p.Hours
-from assignedto a, scientists s, projects p 
-where a.Project = p.Code 
-and a.Scientist = s.SSN
-order by p.Name ASC, s.Name ASC;
+SELECT s.Name, p.Name, p.Hours
+FROM assignedto a, scientists s, projects p 
+WHERE a.Project = p.Code 
+AND a.Scientist = s.SSN
+ORDER BY p.Name ASC, s.Name ASC;
 
 
 -- 6.2 Select the project names which are not assigned yet
-select * 
-from (select * from projects p left outer join assignedto a on p.Code = a.Project) x
-where Scientist is null;
+SELECT  * 
+FROM 
+    (SELECT * 
+    FROM projects p 
+    LEFT OUTER JOIN  assignedto a ON p.Code = a.Project) x
+WHERE Scientist IS NULL;
 
-select * from projects p where p.Code not in (select distinct Project from assignedto)
+SELECT * 
+    FROM projects p 
+    WHERE p.Code NOT IN 
+    (
+        SELECT DISTINCT Project 
+        FROM assignedto)
+
