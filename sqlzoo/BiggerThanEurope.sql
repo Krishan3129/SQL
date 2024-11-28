4628f139b618f92d3e53c0afe92397b902a1847c
Krishan Kumar Choudhary <140525583+Krishan3129@users.noreply.github.com>
2024-11-29 00:36:35 +0530

    Update BiggerThanEurope.sql

--- sqlzoo/BiggerThanEurope.sql
+++ sqlzoo/BiggerThanEurope.sql
@@ -1,7 +1,8 @@
 SELECT name 
 FROM world
-WHERE gdp > 
-	  (SELECT 
-	   MAX(gdp) 
-	   FROM world 
-	   WHERE continent LIKE 'Europe')
+WHERE GDP > 
+	  (
+		SELECT MAX(gdp) 
+	  	FROM world 
+	   	WHERE continent LIKE 'Europe'
+	)
