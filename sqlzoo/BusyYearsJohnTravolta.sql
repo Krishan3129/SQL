68c56b22d060dd5b434849dd95ba69a429bfd547
Krishan Kumar Choudhary <140525583+Krishan3129@users.noreply.github.com>
2024-12-01 01:31:56 +0530

    Update BusyYearsJohnTravolta.sql

--- sqlzoo/BusyYearsJohnTravolta.sql
+++ sqlzoo/BusyYearsJohnTravolta.sql
@@ -1,20 +1,22 @@
-SELECT yr,COUNT(title) 
+SELECT yr, COUNT ( title ) 
 FROM movie 
 JOIN casting 
-	ON movie.id=movieid
+	ON movie.id = movieid
 JOIN actor 
 	ON actorid=actor.id
-where name='John Travolta'
+where name = 'John Travolta'
 GROUP BY yr
-HAVING COUNT(title)=
-(SELECT MAX(c) 
- FROM
-	(SELECT yr,COUNT(title) AS c 
-	 FROM movie 
-	 JOIN casting 
-	 ON movie.id=movieid
-	 JOIN actor
-	 ON actorid=actor.id
- WHERE name='John Travolta'
- GROUP BY yr) AS t
-)
+HAVING COUNT ( title ) =
+	(
+	SELECT MAX(c) 
+	FROM 
+		(
+		SELECT yr,COUNT(title) AS c 
+	 	FROM movie 
+	 	JOIN casting 
+	 	ON movie.id=movieid
+	 	JOIN actor
+	 	ON actorid=actor.id
+ 		WHERE name='John Travolta'
+ 		GROUP BY yr) AS t
+	)
