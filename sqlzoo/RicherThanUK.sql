1b0da741ae594d43f73cae0fcc12cf9db5e2cad7
Krishan Kumar Choudhary <140525583+Krishan3129@users.noreply.github.com>
2024-12-04 23:24:41 +0530

    Update RicherThanUK.sql

--- sqlzoo/RicherThanUK.sql
+++ sqlzoo/RicherThanUK.sql
@@ -1,7 +1,7 @@
 SELECT name 
 FROM world
-WHERE continent LIKE 'Europe'
-AND gdp / population > 
-(SELECT gdp / population 
+WHERE continent LIKE 'Europe'AND gdp / population > (
+ SELECT gdp / population 
  FROM world
- WHERE name LIKE 'United Kingdom')
+ WHERE name LIKE 'United Kingdom'
+ )
