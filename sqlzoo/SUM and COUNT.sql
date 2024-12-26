df5492e29770a55c20f564c00fb3a6b8072ad9ea
Krishan Kumar Choudhary <140525583+Krishan3129@users.noreply.github.com>
2024-12-26 22:22:56 +0530

    Update SUM and COUNT.sql

--- sqlzoo/SUM and COUNT.sql
+++ sqlzoo/SUM and COUNT.sql
@@ -1,8 +1,21 @@
-SELECT 
-  game.mdate, eteam.teamname
-FROM 
-  game 
-JOIN eteam 
-ON (
-  game.team1 = eteam.id)
-WHERE eteam.coach like 'Fernando Santos'
+-- Contents
+-- name	continent	area	population	gdp
+-- Afghanistan	Asia	652230	25500100	20343000000
+-- Albania	Europe	28748	2831741	12960000000
+-- Algeria	Africa	2381741	37100000	188681000000
+-- Andorra	Europe	468	78115	3712000000
+-- Angola	Africa	1246700	20609294	100990000000
+
+-- 1	World Country Profile: Aggregate functions
+
+
+-- 2	You might want to look at these examples first
+-- 3	Total world population
+-- 4	List of continents
+-- 5	GDP of Africa
+-- 6	Count the big countries
+-- 7	Baltic states population
+-- 8	Using GROUP BY and HAVING
+-- 9	Counting the countries of each continent
+-- 10	Counting big countries in each continent
+-- 11	Counting big continents
