2839ce9065c7d44d6c00af9a582741ea9d991691
Krishan Kumar Choudhary <140525583+Krishan3129@users.noreply.github.com>
2024-12-26 22:25:34 +0530

    Update SUM and COUNT.sql

--- sqlzoo/SUM and COUNT.sql
+++ sqlzoo/SUM and COUNT.sql
@@ -6,13 +6,16 @@
 -- Andorra	Europe	468	78115	3712000000
 -- Angola	Africa	1246700	20609294	100990000000
 
--- 1	World Country Profile: Aggregate functions
+-- 1	Show the total population of the world.
+SELECT SUM(population)
+FROM world
 
 
--- 2	You might want to look at these examples first
--- 3	Total world population
--- 4	List of continents
--- 5	GDP of Africa
+
+-- 2	List all the continents - just once each.
+-- 3	Give the total GDP of Africa
+-- 4	How many countries have an area of at least 1000000
+-- 5	What is the total population of ('Estonia', 'Latvia', 'Lithuania')
 -- 6	Count the big countries
 -- 7	Baltic states population
 -- 8	Using GROUP BY and HAVING
