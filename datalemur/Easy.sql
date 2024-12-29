337c7eaac61d463b41347077986a461fca969126
Krishan Kumar Choudhary <140525583+Krishan3129@users.noreply.github.com>
2024-12-29 22:09:46 +0530

    Update Easy.sql

--- datalemur/Easy.sql
+++ datalemur/Easy.sql
@@ -1,25 +1,32 @@
--- game
--- id	mdate	stadium	team1	team2
--- 1001	8 June 2012	National Stadium, Warsaw	POL	GRE
--- 1002	8 June 2012	Stadion Miejski (Wroclaw)	RUS	CZE
--- 1003	12 June 2012	Stadion Miejski (Wroclaw)	GRE	CZE
--- 1004	12 June 2012	National Stadium, Warsaw	POL	RUS
--- ...
+-- Assume you're given a table Twitter tweet data, write a query to obtain a histogram of tweets posted per user in 2022. 
+-- Output the tweet count per user as the bucket and the number of Twitter users who fall into that bucket.
 
+-- In other words, group the users by the number of tweets they posted in 2022 and count the number of users in each group.
 
--- goal
--- matchid	teamid	player	gtime
--- 1001	POL	Robert Lewandowski	17
--- 1001	GRE	Dimitris Salpingidis	51
--- 1002	RUS	Alan Dzagoev	15
--- 1002	RUS	Roman Pavlyuchenko	82
--- ...
+-- tweets Table:
+-- Column Name	Type
+-- tweet_id	integer
+-- user_id	integer
+-- msg	string
+-- tweet_date	timestamp
+-- tweets Example Input:
+-- tweet_id	user_id	msg	tweet_date
+-- 214252	111	Am considering taking Tesla private at $420. Funding secured.	12/30/2021 00:00:00
+-- 739252	111	Despite the constant negative press covfefe	01/01/2022 00:00:00
+-- 846402	111	Following @NickSinghTech on Twitter changed my life!	02/14/2022 00:00:00
+-- 241425	254	If the salary is so competitive why won’t you tell me what it is?	03/01/2022 00:00:00
+-- 231574	148	I no longer have a manager. I can't be managed	03/23/2022 00:00:00
+-- Example Output:
+-- tweet_bucket	users_num
+-- 1	2
+-- 2	1
+-- Explanation:
+-- Based on the example output, there are two users who posted only one tweet in 2022, and one user who posted two tweets in 2022. 
+-- The query groups the users by the number of tweets they posted and displays the number of users in each group.
 
-
--- eteam
--- id	teamname	coach
--- POL	Poland	Franciszek Smuda
--- RUS	Russia	Dick Advocaat
--- CZE	Czech Republic	Michal Bilek
--- GRE	Greece	Fernando Santos
--- ...
+select tweet_bucket, count(*) as users_num 
+FROM 
+    (SELECT user_id, count(*) as tweet_bucket FROM tweets 
+    where tweet_date Between '01/01/2022'  and '12/31/2022'
+    group by user_id) as a 
+group by tweet_bucket order by 1 ;
