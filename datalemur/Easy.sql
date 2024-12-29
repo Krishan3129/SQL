28e9675788d21c7c2a7097c7159e3c090ca0a89e
Krishan Kumar Choudhary <140525583+Krishan3129@users.noreply.github.com>
2024-12-30 00:49:17 +0530

    Update Easy.sql

--- datalemur/Easy.sql
+++ datalemur/Easy.sql
@@ -141,3 +141,81 @@ ORDER by a.page_id ASC
 SELECT part, assembly_step 
 FROM parts_assembly
 where finish_date is NULL
+
+
+-- Assume you're given the table on user viewership categorised by device type where the three types are laptop, tablet, and phone.
+
+-- Write a query that calculates the total viewership for laptops and mobile devices where mobile is defined as the sum of tablet and phone viewership. 
+-- Output the total viewership for laptops as laptop_reviews and the total viewership for mobile devices as mobile_views.
+
+-- Effective 15 April 2023, the solution has been updated with a more concise and easy-to-understand approach.
+
+-- viewership Table
+-- Column Name	Type
+-- user_id	integer
+-- device_type	string ('laptop', 'tablet', 'phone')
+-- view_time	timestamp
+-- viewership Example Input
+-- user_id	device_type	view_time
+-- 123	tablet	01/02/2022 00:00:00
+-- 125	laptop	01/07/2022 00:00:00
+-- 128	laptop	02/09/2022 00:00:00
+-- 129	phone	02/09/2022 00:00:00
+-- 145	tablet	02/24/2022 00:00:00
+-- Example Output
+-- laptop_views	mobile_views
+-- 2	3
+    
+SELECT 
+(SELECT
+COUNT(user_id) 
+FROM viewership
+WHERE device_type='laptop') as laptop_views,
+
+COUNT(user_id) as mobile_views 
+FROM viewership
+WHERE device_type in ('tablet', 'phone');
+
+
+
+-- OR 
+
+
+    
+SELECT 
+Sum (CASE When device_type='laptop' then 1 else 0 END) as laptop_views,
+Sum (CASE When device_type IN ('phone','tablet') then 1 else 0 END) as mobile_views
+FROM viewership;
+
+
+
+
+-- Given a table of Facebook posts, for each user who posted at least twice in 2021, 
+-- write a query to find the number of days between each user’s first post of the year and last post of the year in the year 2021. 
+-- Output the user and number of the days between each user's first and last post.
+
+-- p.s. If you've read the Ace the Data Science Interview and liked it, consider writing us a review?
+
+-- posts Table:
+-- Column Name	Type
+-- user_id	integer
+-- post_id	integer
+-- post_content	text
+-- post_date	timestamp
+-- posts Example Input:
+-- user_id	post_id	post_content	post_date
+-- 151652	599415	Need a hug	07/10/2021 12:00:00
+-- 661093	624356	Bed. Class 8-12. Work 12-3. Gym 3-5 or 6. Then class 6-10. Another day that's gonna fly by. I miss my girlfriend	07/29/2021 13:00:00
+-- 004239	784254	Happy 4th of July!	07/04/2021 11:00:00
+-- 661093	442560	Just going to cry myself to sleep after watching Marley and Me.	07/08/2021 14:00:00
+-- 151652	111766	I'm so done with covid - need travelling ASAP!	07/12/2021 19:00:00
+-- Example Output:
+-- user_id	days_between
+-- 151652	2
+-- 661093	21
+
+SELECT user_id, EXTRACT(day from (max(post_date)-min(post_date))) as days_between
+FROM posts
+where post_date between '01/01/2021' and '12/31/2021'
+GROUP by user_id
+having count(post_date)>1
