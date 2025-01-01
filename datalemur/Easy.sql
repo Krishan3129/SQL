d9bed75373ce4a6d865f7220d58a31c9f3a6a8bb
Krishan Kumar Choudhary <140525583+Krishan3129@users.noreply.github.com>
2025-01-02 01:26:03 +0530

    Update Easy.sql

--- datalemur/Easy.sql
+++ datalemur/Easy.sql
@@ -438,3 +438,21 @@ def contains_duplicate(input)-> bool:
     return False
   else:
     return True
+
+
+
+
+-- Given an array containing exactly three unique points, each represented as coordinate pairs (x, y) with no duplicates, 
+-- determine whether these points lie on the same straight line. Return True if they do, and False otherwise.
+
+def on_straight_line(points):
+  
+  a, b = points[0]
+  m, n = points[1]
+  x, y = points[2]
+	
+  if (n-b) * (x-m) == (y-n) * (m-a):
+    return True
+	  
+  else:
+    return False
