3ad274adf58cf04897ea975c00d06a10b1e67468
Krishan Kumar Choudhary <140525583+Krishan3129@users.noreply.github.com>
2025-01-02 20:48:18 +0530

    Update Easy.sql

--- datalemur/Easy.sql
+++ datalemur/Easy.sql
@@ -456,3 +456,19 @@ def on_straight_line(points):
 	  
   else:
     return False
+
+-- our team at JPMorgan Chase is preparing to launch a new credit card, and to gain some insights, you're analyzing how many credit cards were issued each month.
+
+-- Write a query that outputs the name of each credit card and the difference in the number of issued cards between the month with the highest issuance cards and the lowest issuance. Arrange the results based on the largest disparity.
+
+-- monthly_cards_issued Table:
+-- Column Name	Type
+-- card_name	string
+-- issued_amount	integer
+-- issue_month	integer
+-- issue_year	integer
+
+SELECT card_name, MAX(issued_amount)-MIN(issued_amount) as difference
+FROM monthly_cards_issued
+group by card_name
+order by difference DESC
