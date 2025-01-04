9af72888bb7f0a0911adb34fa0865a4c5c7811bb
Krishan Kumar Choudhary <140525583+Krishan3129@users.noreply.github.com>
2025-01-04 21:38:14 +0530

    Update Easy.sql

--- datalemur/Easy.sql
+++ datalemur/Easy.sql
@@ -484,3 +484,23 @@ SELECT
       END)
     /COUNT(*), 1) AS uncategorised_call_pct
 FROM callers
+
+
+
+-- Assume you have an events table on Facebook app analytics. 
+-- Write a query to calculate the click-through rate (CTR) for the app in 2022 and round the results to 2 decimal places.
+
+-- Definition and note:
+
+-- Percentage of click-through rate (CTR) = 100.0 * Number of clicks / Number of impressions
+-- To avoid integer division, multiply the CTR by 100.0, not 100.\
+
+
+SELECT 
+  app_id,
+ ROUND(100.0 *
+    sum(case WHEN event_type = 'click' then 1 ELSE 0 END) / 
+    sum(case WHEN event_type = 'impression' then 1 ELSE 0 END),2) as ctr
+FROM events
+WHERE timestamp BETWEEN '2022-01-01' AND '2022-12-31'
+GROUP BY app_id;;
