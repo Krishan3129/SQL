d99e0f13496a98aab3875ed9de18c5376882d6a5
Krishan Kumar Choudhary <140525583+Krishan3129@users.noreply.github.com>
2025-01-05 00:17:06 +0530

    Update Easy.sql

--- datalemur/Easy.sql
+++ datalemur/Easy.sql
@@ -532,3 +532,40 @@ left JOIN texts t
 ON e.email_id = t.email_id
 group by e.user_id
 having count(t.email_id)=2
+
+
+
+-- IBM is analyzing how their employees are utilizing the Db2 database by tracking the SQL queries executed by their employees. 
+-- The objective is to generate data to populate a histogram that shows the number of unique queries run by employees during the third quarter of 2023 (July to September). Additionally, it should count the number of employees who did not run any queries during this period.
+
+-- Display the number of unique queries as histogram categories, along with the count of employees who executed that number of unique queries.
+
+-- queries Schema:
+-- Column Name	Type	Description
+-- employee_id	integer	The ID of the employee who executed the query.
+-- query_id	integer	The unique identifier for each query (Primary Key).
+-- query_starttime	datetime	The timestamp when the query started.
+-- execution_time	integer	The duration of the query execution in seconds.
+
+
+
+-- employees Schema:
+-- Assume that the table below displays all employees in the table:
+
+-- Column Name	Type	Description
+-- employee_id	integer	The ID of the employee who executed the query.
+-- full_name	string	The full name of the employee.
+-- gender	string	The gender of the employee.
+
+SELECT distinct_queries, COUNT(employee_id) AS employee_count
+FROM (
+  SELECT e.employee_id, 
+         COUNT(DISTINCT q.query_id) as distinct_queries
+  FROM employees e
+  LEFT JOIN queries q 
+  ON e.employee_id = q.employee_id
+  AND EXTRACT(MONTH FROM q.query_starttime) BETWEEN 7 AND 9
+  GROUP BY e.employee_id
+) AS employee_queries
+GROUP BY distinct_queries
+ORDER BY distinct_queries;
