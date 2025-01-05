445fefb18f15f1e4da8201e65aa3e74250316361
Krishan Kumar Choudhary <140525583+Krishan3129@users.noreply.github.com>
2025-01-05 22:55:26 +0530

    Update Medium Level.sql

--- datalemur/Medium Level.sql
+++ datalemur/Medium Level.sql
@@ -29,3 +29,30 @@ where t.trans_no = 3
 
 
 
+-- Imagine you're an HR analyst at a tech company tasked with analyzing employee salaries. 
+-- Your manager is keen on understanding the pay distribution and asks you to determine the second highest salary among all employees.
+
+-- It's possible that multiple employees may share the same second highest salary. In case of duplicate, display the salary only once.
+
+-- employee Schema:
+-- column_name	type	description
+-- employee_id	integer	The unique ID of the employee.
+-- name	string	The name of the employee.
+-- salary	integer	The salary of the employee.
+-- department_id	integer	The department ID of the employee.
+-- manager_id	integer	The manager ID of the employee.
+
+SELECT salary
+FROM employee
+order by salary DESC
+OFFSET 1
+limit 1
+
+-- or
+
+select max(salary) 
+from employee 
+where salary not in (select max(salary) from employee)
+
+
+
