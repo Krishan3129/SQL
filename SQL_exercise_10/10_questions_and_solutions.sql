0ebdde66499ac9ed4a7d06650be4147e60d20a31
Krishan Kumar Choudhary <140525583+Krishan3129@users.noreply.github.com>
2025-01-20 21:54:52 +0530

    Update 10_questions_and_solutions.sql

--- SQL_exercise_10/10_questions_and_solutions.sql
+++ SQL_exercise_10/10_questions_and_solutions.sql
@@ -1,5 +1,5 @@
 -- 10.1 Join table PEOPLE and ADDRESS, but keep only one address information for each person (we don't mind which record we take for each person). 
--- i.e., the joined table should have the same number of rows as table PEOPLE
+-- the joined table should have the same number of rows as table PEOPLE
 
 SELECT
 PEOPLE.id, PEOPLE.name, TEMP.address
@@ -16,7 +16,7 @@ ON PEOPLE.id = TEMP.id;
 
 
 -- 10.2 Join table PEOPLE and ADDRESS, but ONLY keep the LATEST address information for each person. 
--- i.e., the joined table should have the same number of rows as table PEOPLE
+-- the joined table should have the same number of rows as table PEOPLE
 
 SELECT
 PEOPLE.id, PEOPLE.name, TEMP.address
